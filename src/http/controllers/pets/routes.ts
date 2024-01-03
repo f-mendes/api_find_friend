0c76daccec502fce11f6bae59eb3ae41c7952e85
Felipe <fsoares@autodoc.com.br>
2024-01-03 17:51:53 -0300

    feat: add authentication to route of pets

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { uploadImages } from '@/http/middlewares/upload-images'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 const upload = uploadImages()
 
 export async function petsRoutes(app: FastifyInstance) {
-  app.post('/pets', { preHandler: upload.array('petImage', 4) }, create)
+  app.post(
+    '/pets',
+    { onRequest: [verifyJwt], preHandler: upload.array('petImage', 4) },
+    create,
+  )
 }
